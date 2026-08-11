2e1a918b71c4c9680c22c23dbdc0046c4be233c8
Dev <debabrata13990872@gmail.com>
2026-08-11 16:27:12 +0530

    Added product detail navigation when clicked

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,11 +1,21 @@
+import { Link } from "react-router-dom";
+
 function ProductCard({ product }) {
   return (
-    <div className="w-=full p-4 space-y-1 rounded-xl shadow-md bg-orange-300">
+    <Link to={`/products/${product.id}`}>
+    
+    <div className="w-full p-4 space-y-1 rounded-xl shadow-md bg-orange-300">
+      
       <img className="w-full h-64 object-cover" src={product.image} alt={product.name} />
+      
       <h2 className="text-xl font-semibold text-amber-800">{product.name}</h2>
+      
       <p className="font-bold text-cyan-900">₹{product.price}</p>
+      
       <p>{product.category}</p>
+    
     </div>
+    </Link>
   );
 }
 
